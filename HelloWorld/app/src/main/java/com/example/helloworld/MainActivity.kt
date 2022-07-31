d7add3bc36e575436e7afac49f0ec6246d4bc034
Sam <pbcmarsh@gmail.com>
2022-07-31 18:09:38 -0500

    user can press button to change background color

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.style.BackgroundColorSpan
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
 
 // This Kotlin file is where we're going to be handling user interaction
@@ -18,15 +20,27 @@ class MainActivity : AppCompatActivity() {
         // User can tap a button to change the text color of the label.
         // 1. Add a button to our layout
 
+
+        /**
+         Change text color button
+         */
         // Get a reference to button
         // 2. Set up logical to know when user has tapped on button
-        findViewById<Button>(R.id.button).setOnClickListener {
+        findViewById<Button>(R.id.buttonTextColor).setOnClickListener {
             // Handles button tap
             // 2. Change the color of the text
             Log.i("Sam", "Button is Working")
             // 1. Get a reference to the text view
             // 2. Set the color fo the text view
             findViewById<TextView>(R.id.textView).setTextColor(getResources().getColor(R.color.red))
+        }
+
+        /**
+         Change view color button
+         */
+        findViewById<Button>(R.id.buttonViewColor).setOnClickListener {
+            Log.i("Sam", "Button is Working")
+            findViewById<ConstraintLayout>(R.id.background).setBackgroundColor(getResources().getColor(R.color.green))
         }
     }
 }
